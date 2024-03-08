50e4b0a49af2ed7e50f3fcec3d7faa087268c236
Gabriel Silva Ezequiel <77858768+Gabrie345@users.noreply.github.com>
2024-03-08 19:37:36 -0300

    formulario

---
+++ js/contato.js
@@ -0,0 +1,5 @@
+const nome = document.getElementById('nome').value.trim();
+const email = document.getElementById('email').value.trim();
+const telefone = document.getElementById('telefone').value.trim();
+const mensagem = document.getElementById('mensagem').value.trim();
+let formValido = true;
